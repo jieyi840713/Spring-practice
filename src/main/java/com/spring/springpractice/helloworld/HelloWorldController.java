26fb044a49cceba52c3dd0f4bd2b699b57b25dcd
Jay <jieyi840713@gmail.com>
2023-01-09 20:04:43 +0800

    Exploring Internationalization for REST API

--- src/main/java/com/spring/springpractice/helloworld/HelloWorldController.java
+++ src/main/java/com/spring/springpractice/helloworld/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.spring.springpractice.helloworld;
 
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+import org.springframework.context.MessageSource;
 
 @RestController
 public class HelloWorldController {
@@ -18,4 +22,23 @@ public class HelloWorldController {
     }
 
 
+
+        private MessageSource messageSource;
+
+        public HelloWorldController(MessageSource messageSource) {
+            this.messageSource = messageSource;
+        }
+
+        @GetMapping(path = "/hello-world-internationalized")
+        public String helloWorldInternationalized() {
+            Locale locale = LocaleContextHolder.getLocale();
+            return messageSource.getMessage("good.morning.message", null, "Default Message", locale );
+            //1:
+            //2:
+//		- Example: `en` - English (Good Morning)
+//		- Example: `nl` - Dutch (Goedemorgen)
+//		- Example: `fr` - French (Bonjour)
+//		- Example: `de` - Deutsch (Guten Morgen)
+
+        }
 }
