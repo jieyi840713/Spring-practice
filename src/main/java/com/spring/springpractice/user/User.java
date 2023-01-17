a827a670ba10305dacf82374cc0a97f5212a4c36
Jay <jieyi840713@gmail.com>
2023-01-17 11:41:30 +0800

    Creating Post Entity with Mant to One Relationship with User Entity

--- src/main/java/com/spring/springpractice/user/User.java
+++ src/main/java/com/spring/springpractice/user/User.java
@@ -1,19 +1,23 @@
 package com.spring.springpractice.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity(name = "user_details")
 public class User {
     @Id
     @GeneratedValue
     private int id;
+
     @Size(min=2, message = "Name should have at least 2 characters")
 //    @JsonProperty("user_name")
     private String name;
@@ -21,6 +25,10 @@ public class User {
     @Past(message = "Birth Date should be in the past")
 //    @JsonProperty("birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> post;
 
     public User(int id, String name, LocalDate birthDate) {
         this.id = id;
